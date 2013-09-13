c6e7043917076cee4f6769a9039d734145c9a562
Teruo Morimoto <Teruo Morimoto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-13 15:36:40 +0000

    Version　1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/lesson-scheduler/trunk@771706 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/lesson_scheduler.js
+++ js/lesson_scheduler.js
@@ -1,9 +1,8 @@
-jQuery(function(){
+jQuery(function($){
     $( "#lesson_datepicker" ).datepicker();
 });
 
-jQuery(function(){
+jQuery(function($){
     $(".tablesorter-1").tablesorter({widgets: ['zebra'] }); 
     $(".tablesorter-2").tablesorter({widgets: ['zebra'] }); 
 });
-
